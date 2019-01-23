6174ed06c941a130ed338d38966996350f3f7a8d
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-23 17:53:15 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def mealChoice(meal_choice="meat")
-  puts meal_choice
+def #meal_choice(food="meat")
+  puts food
 end
 
-mealChoice("vegan")
+meal_choice("vegan")
